ef81f03c6885ca92d797f7cc9a9cef434d38e28b
Marguerite Blair <marguerite.blair@gmail.com>
2020-07-24 07:15:58 -0400

    js modules and the require method

---
+++ count.js
@@ -0,0 +1,7 @@
+let counter = function(arr) {
+    return 'There are ' + arr.length + " elements in this array."
+}
+
+console.log(counter(['Margie', 'Chelsea', 'Will']))
+
+module.exports = counter; 
