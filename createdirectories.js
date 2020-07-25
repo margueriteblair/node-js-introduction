60cde02dc62f134c9cf966073084d07398e02175
Marguerite Blair <marguerite.blair@gmail.com>
2020-07-25 09:41:01 -0400

    removed stuff dir and txt files

--- createdirectories.js
+++ createdirectories.js
@@ -4,15 +4,15 @@ let fs = require('fs');
 //fs.unlinkSync('writeMe.txt');// in order to avoid using a callback function, use the syncronous methods
 
 
-    fs.readFile('read.txt', 'utf8', function(error, data) {
-        fs.writeFileSync('./stuff/writeMe.txt', data)
-    })
+    // fs.readFile('read.txt', 'utf8', function(error, data) {
+    //     fs.writeFileSync('./stuff/writeMe.txt', data)
+    // })
 
 
-
-//fs.rmdirSync('stuff')
 //fs.unlink('writeMe.txt');
-//fs.unlink('./stuff/writeMe.txt', function() {
-//     fs.rmdir('stuff');
-// })
+fs.rmdirSync('stuff'); //no callback needed
+//fs.rmdir('stuff' function() {
+    //this requires a callback
+//})
+
 //fs.rmdir('stuff')
