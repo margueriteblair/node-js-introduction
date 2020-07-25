68a83f7bc5bcc7ac9f90789f5d6f39d4ff6fb8ac
Marguerite Blair <marguerite.blair@gmail.com>
2020-07-25 17:31:26 -0400

    commented notes on streams and buffers

---
+++ streamsandbuffers.js
@@ -0,0 +1,9 @@
+//a buffer is a temp storage spot for a chunk of data that is being passed from one place to another
+//the buffer is filled with data and then passed a long
+//with buffers you transfer small chunks of data at a time
+
+//small buffer storage fills up and then passes on
+//streams is a stream of data that flows from one place to a buffer, then goes on to be processed
+//chunks of data can be processed bit by bit
+//in node js we can create streamsto transfer data
+//this increases performance
