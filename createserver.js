69d3a3f81f89c0b3fef284951c7749caff0b8dd8
Marguerite Blair <marguerite.blair@gmail.com>
2020-07-25 18:09:45 -0400

    used the browser as a writestream

--- createserver.js
+++ createserver.js
@@ -1,13 +1,15 @@
 //create a server in node js
 
 let http = require('http');
+let fs = require('fs')
 
 let server = http.createServer(function(req, response) {
     //response header 
     //content-type & status
     console.log('request was made: ' + req.url)
     response.writeHead(200, ('Content-Type', 'text/plain'));
-    response.end('Hey Margie'); //message to send when the response is done
+    var myReadStream = fs.createReadStream(__dirname + '/caesar.txt', 'utf8');
+    myReadStream.pipe(response); //message to send when the response is done
 });
 
 server.listen(3000, '127.0.0.1');
--- readablestreams.js
+++ readablestreams.js
@@ -6,13 +6,18 @@ let http = require('http');
 let fs = require('fs');
 
 var myReadStream = fs.createReadStream(__dirname + '/caesar.txt', 'utf8');
-var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
+var myWriteStream = fs.createWriteStream(__dirname + '/writeMe2.txt');
 //we have to specify uft8 so that we can have it translated from the buffer encoded version to the regular charactersk
 //this will pass the data and split it up into chunks
 //event called 'data' built into createReadStream
 
-myReadStream.on('data', function(chunk){
-    console.log('new chunk received');
-    //console.log(chunk);
-    myWriteStream.write(chunk)
-})
+// myReadStream.on('data', function(chunk){
+//     console.log('new chunk received');
+//     //console.log(chunk);
+//     myWriteStream.write(chunk)
+// })
+
+//pipes - pipes read streams into a write stream
+//pipes 
+
+myReadStream.pipe(myWriteStream);
