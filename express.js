c9ef37c064db2d59ceafc8e09d001a12ae29f88d
Marguerite Blair <marguerite.blair@gmail.com>
2020-07-26 09:39:37 -0400

    templating engines

--- express.js
+++ express.js
@@ -1,15 +1,20 @@
 let express = require('express');
 let app = express();
+
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
-    res.send('this is the homepage');
+    res.sendFile(__dirname + '/index.html');
 })
 app.get('/contact', function(req, res) {
-    res.send('this is the contact page');
+    res.sendFile(__dirname + '/contact.html');
 })
 
-app.get('/profile/:id', function(request, response) {
-    response.send('You requested to see a profile with the id of ' + request.params.id);
+app.get('/profile/:name', function(request, response) {
+    let data = {age: 21, job: 'ninja'};
+    response.render('profile', {person: request.params.name, data: data});
 })
+//the route is still dynamic and it changes
 app.listen(3000) //app listens for the specific port number
 //http methods, get, post, delete, put
 //there are different methods to respond to request
