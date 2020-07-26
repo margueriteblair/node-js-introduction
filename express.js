3a9c60698b4115df5cfcb223c0b37f7bf60dd470
Marguerite Blair <marguerite.blair@gmail.com>
2020-07-26 08:56:06 -0400

    static routes

--- express.js
+++ express.js
@@ -4,7 +4,7 @@ app.get('/', function(req, res) {
     res.send('this is the homepage');
 })
 app.get('/contact', function(req, res) {
-    res.send('this is the homepage');
+    res.send('this is the contact page');
 })
 app.listen(3000) //app listens for the specific port number
 //http methods, get, post, delete, put
