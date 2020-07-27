ce830d8c85052654ebec348fd872f79608f6eaf7
Marguerite Blair <marguerite.blair@gmail.com>
2020-07-27 19:52:09 -0400

    first npm project with express and mongoose

--- firstnpm/app.js
+++ firstnpm/app.js
@@ -1,2 +1,19 @@
-console.log('testing testing 123')
+const express = require('express');
+const mongoose = require('mongoose');
+
+//console.log(express);
+const port = 3000;
+const app = express(); //app 
+
+app.get('/', (req, res) => { //the forward slash is just the root route
+    console.log(req);
+    res.send('Hello World')
+    //res.send(alert(`This is an alert!`));
+})
+app.listen(port, () => {
+    console.log(`Server is now listening on port ${port}`);
+    
+}); //3000 is standard port number
+
+
 
