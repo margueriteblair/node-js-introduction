66bc94566d5cbe4c2ab2a10487191e3d033df5c9
Marguerite Blair <marguerite.blair@gmail.com>
2020-07-28 10:41:03 -0400

    comments on line 6

--- firstnpm/app.js
+++ firstnpm/app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 //console.log(express);
 const port = 3000;
-const app = express(); //app 
+const app = express(); //app is a commonplace name for this stuff
 
 app.get('/', (req, res) => { //the forward slash is just the root route
     console.log(req);
