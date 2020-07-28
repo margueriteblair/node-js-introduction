88de30b467f463242178bec402b32c17933f23cf
Marguerite Blair <marguerite.blair@gmail.com>
2020-07-28 12:36:25 -0400

    app is listening for server port 3000

--- secondnpm/app.js
+++ secondnpm/app.js
@@ -1,1 +1,18 @@
-console.log('Hello World, this is an npm test');
+//console.log('Hello World, this is an npm test');
+
+let fs = require('fs');
+let express = require('express');
+let mongoose = require('mongoose');
+
+let app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    //console.log(req);
+    res.send('This is the homepage');
+})
+
+app.listen(port, () => {
+    console.log(`The server is now successfully listening to port ${port}`)
+});
+
