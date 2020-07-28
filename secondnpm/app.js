cf2f1d1b1d24c2776a4ad6c2914031d7e68aceb8
Marguerite Blair <marguerite.blair@gmail.com>
2020-07-28 16:25:12 -0400

    added labels to the html

--- secondnpm/app.js
+++ secondnpm/app.js
@@ -1,10 +1,9 @@
 //console.log('Hello World, this is an npm test');
-
 let fs = require('fs');
 let express = require('express');
 let mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-
+let bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({extended: false});
 let app = express();
 const port = 3000;
 
@@ -12,13 +11,21 @@ app.get('/', (req, res) => {
     //console.log(req);
     res.sendFile(__dirname + '/index.html');
 })
-app.get('/contact', (req, res) => {
+app.get('/contact', urlencodedParser, (req, res) => {
+    console.log(req.body);
     //res.send('This is the page for Margies contact information')
     res.sendFile(__dirname + '/contact.html');
 })
+app.post('/contact', (req, res) => {
+    //res.send('This is the page for Margies contact information')
+    res.sendFile(__dirname + '/contact.html');
+})
+
 app.get('/profile/:id', (req, res) => {
     res.send(`You requested to see a profile with the id of ${req.params.id}`)
 }) //dynamic routes
+
+
 
 app.listen(port, () => {
     console.log(`The server is now successfully listening to port ${port}`)
