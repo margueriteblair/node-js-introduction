854ebe06835d34f1ab3f381c2292550d526b326e
Marguerite Blair <marguerite.blair@gmail.com>
2020-07-29 12:05:29 -0400

    fixed bug where :id returned undefined

--- fourthnpm/app.js
+++ fourthnpm/app.js
@@ -4,19 +4,19 @@ let express = require('express');
 let app = express();
 //imported middleware and routers
 const firstMid = require('./middleware/firstmiddleware');
-const router = require('./routes/userrouter')
+const userRouter = require('./routes/userrouter')
 const homeRouter = require('./routes/homerouter');
 app.set('view engine', 'ejs'); //we're setting the view enginge/templating engine to be ejs
 
 const port = 3000;
 
 app.use(firstMid)
-app.use('/profile/:id', router)
+app.use('/:id', userRouter)
 app.use('/', homeRouter);
 
 
 app.listen(port, function() {
-    console.log('success to listen to port 3000');
+    console.log('success to listen to port ' + port);
 });
 
 
--- fourthnpm/routes/userrouter.js
+++ fourthnpm/routes/userrouter.js
@@ -1,8 +1,7 @@
 const express = require('express');
 
-
 router = express.Router();
-router.get('/profile/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     res.send(`The user is requesting to see ${req.params.id}. The user will also have access to ${req.params.id}`);
 })
 
