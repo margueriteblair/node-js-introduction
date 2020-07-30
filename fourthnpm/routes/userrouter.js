8755fd05b1f8498efc6c74050b96badf8d728fe1
Marguerite Blair <marguerite.blair@gmail.com>
2020-07-30 06:23:13 -0400

    update

--- fourthnpm/routes/userrouter.js
+++ fourthnpm/routes/userrouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 router = express.Router();
 router.get('/:id', (req, res) => {
-    let data = {age: 21, job: 'careerdevs student'}
+    let data = {age: 21, job: 'careerdevs student', hobbies: ['coding', 'algorithms', 'reading']}
     res.render('profile', {person: req.params.id, data: data} )
 })
 
