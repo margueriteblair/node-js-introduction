a5bbc1ff1ec018d7fdecb5b61c6a1503f8a81bdf
Marguerite Blair <marguerite.blair@gmail.com>
2020-08-03 19:24:35 -0400

    static file middleware

--- usersignup/app.js
+++ usersignup/app.js
@@ -10,6 +10,7 @@ let port = 3000;
 
 //app usage permissions
 app.use(express.json())
+app.use(express.static('public'))
 app.use('/', homeRouter);
 app.use('/user', userpost);
 //middleware that allows us to parse stringified objects
