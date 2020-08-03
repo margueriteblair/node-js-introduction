960e5a42023a666f71e4ec5ab6dfa0a18e95d6c6
Marguerite Blair <marguerite.blair@gmail.com>
2020-08-03 12:30:30 -0400

    edited post route

--- usersignup/routes/userpost.js
+++ usersignup/routes/userpost.js
@@ -3,12 +3,18 @@ let router = express.Router();
 const usernew = require('./usernew');
 
 router.post('/post', (req, res) => {
-    // console.log(req.body)
-    res.sendFile(__dirname + "/usernew.html")
+    console.log(req.body)
+    //res.sendFile(__dirname + "/usernew.html")
+    res.json({
+        Status: 'Successfully made an account!',
+        Username: data.usr,
+        Email: data.email,
+        Password: data.pswd
+    })
 }) 
 //posting is for creating or updating a resource
 //this is why we need to use post to return the submitted information that the user submitted back to them
-
+//always use post if theres sensitive information involved
 router.use('/new', usernew);
 
 module.exports = router;
