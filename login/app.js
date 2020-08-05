40aefd872249bbc97b259ae1129e0ed2d47e0d9c
Marguerite Blair <marguerite.blair@gmail.com>
2020-08-05 11:34:35 -0400

    started xhr req

--- login/app.js
+++ login/app.js
@@ -4,6 +4,8 @@ const express = require('express');
 let app = express();
 let port = 3300;
 
+app.use(express.json())
+
 
 app.listen(port, () => {
     console.log(`Successfully listening to ${port}`)
--- login/login.js
+++ login/login.js
@@ -3,11 +3,13 @@ window.onload = () => {
     const form = document.createElement('form')
     const email = document.createElement('input');
     const password = document.createElement('input')
+    const signInButton = document.createElement('button');
 
     document.body.appendChild(header);
     document.body.appendChild(form);
     form.appendChild(email);
     form.appendChild(password);
+    document.body.appendChild(signInButton);
 
 
     header.id = 'header'
@@ -15,9 +17,26 @@ window.onload = () => {
     form.id = 'form';
     email.id = 'email'
     email.name = 'email'
+    email.type = 'email'
     email.placeholder = 'Email'
     password.id = 'pswd';
+    password.type = 'password'
     password.name = 'pass';
     password.placeholder = 'Password'
+    signInButton.innerText = 'Sign In'
+
+    signInButton.onclick = logData;
+
+    function logData() {
+        console.log('Data was submitted')
+
+        const reqBody = {}; //setting an empty object for the request body
+        const xhr = new XMLHttpRequest();
+        xhr.open('PATCH', PATH)
+        xhr.onload = () => {
+            //log something
+        }
+        xhr.send()
+    }
 
 }
