fd5d46f5863ccfb6e5edfd311d6358b33d43f3ab
Marguerite Blair <marguerite.blair@gmail.com>
2020-08-06 06:53:28 -0400

    added instructions

--- login/app.js
+++ login/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const home = require('./routes/home')
-
+//lets assume the object being updated is only name and email, but a different name and email is being updated each time a new user is logging in
+//post request is for creating new data
 let app = express();
 let port = 3000;
 
 app.use(express.json())
+app.use(express.static('public'))
 app.use('/', home);
 //app.use('/users/login', userRouter)
 
