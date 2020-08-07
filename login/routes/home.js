f200f49b2d8c469e58c978937931e5337f71e8a2
Marguerite Blair <marguerite.blair@gmail.com>
2020-08-06 20:53:00 -0400

    adding get to home route

--- login/routes/home.js
+++ login/routes/home.js
@@ -3,7 +3,16 @@ let express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(process.cwd() + '/public/index.html')
+    res.sendFile(process.cwd() + '/public/index.html') //backend url
 })
+
+//location.origin is the frontend
+router.get(
+    '/login',
+    (req, res) => {
+        const filePath = process.cwd() + 'public/login.html';
+        res.sendFile(filePath);
+    }
+)
 
 module.exports = router;
